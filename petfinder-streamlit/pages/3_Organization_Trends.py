3e4a8e39609e87b59ea434e4c47aa15240148e2f
Jared Kunhart <Jared-Kunhart@proton.me>
2023-05-23 19:36:46 -0500

    visual changes for the chart

--- petfinder-streamlit/pages/3_Organization_Trends.py
+++ petfinder-streamlit/pages/3_Organization_Trends.py
@@ -170,14 +170,31 @@ with org_scatterplot_tab:
     los_by_org_altair_query = """
         SELECT * FROM "%s" %s;
         """ % (pfglobals.DATABASE_TABLE, selected_org_query)
-    # print(los_by_org_altair_query)
+
     altair_df = pfglobals.create_data_frame(pfglobals.run_query(los_by_org_altair_query, pfglobals.conn_dict), "organization_name")
     selected_org_city = altair_df['city'][0]
     selected_org_state = altair_df['state'][0]
+    # scale = alt.Scale(domain=['Female', 'Male'], range=['#7C33FF', '#0867a1'])
     org_chart = alt.Chart(altair_df, title=f'{selected_org} in {selected_org_city}, {selected_org_state}').mark_circle(size=60).encode(
-        x='published_at',
-        y='los',
-        color='gender',
-        tooltip=['name', 'los', 'breed_primary', 'age', 'size']
+        # alt.X('published_at:T').title('hello'),
+        x=alt.X('published_at:T', title='Date Posted'),
+        y=alt.Y('los', title='Length of Stay by Days'),
+        # color=alt.Color('gender', scale=scale),
+        color=alt.Color('gender', title='Gender'),
+        tooltip=[alt.Tooltip('name', title='Name'), alt.Tooltip('los', title='Days Stayed'),
+        alt.Tooltip('breed_primary', title='Breed'), alt.Tooltip('age', title='Age'),
+        alt.Tooltip('size', title='Size')],
+    ).properties(
+        height=500
+    ).configure_legend(
+        titleFontSize=16,
+        labelFontSize=16,
+        orient='top-right'
+    ).configure_title(
+        fontSize=17
+    ).configure_axis(
+        labelFontSize=15,
+        titleFontSize=15,
+        titleFontWeight=600
     ).interactive()
     st.altair_chart(org_chart, use_container_width=True)
