04ff2bd4aee64862d97a811a6e962996d6c4b633
Jhen <zhenjhen192@gmail.com>
2023-07-05 13:51:46 -0500

    removed features in target/binary_cols in config file

--- petfinder-model/config.py
+++ petfinder-model/config.py
@@ -12,9 +12,8 @@ SIZE_DICT={
     'Extra Large': '3'
 }
 
-TARGET_COLS = ["coat", "organization_name", "breed_primary", "breed_secondary", 
-               "color_primary", "color_secondary", "color_tertiary", "city", "state"]
+TARGET_COLS = ["coat", "organization_name", "breed_primary",
+               "color_primary", "city", "state"]
 
-BINARY_COLS = ["breed_mixed", "breed_unknown", "good_with_children", "good_with_dogs", 
-               "good_with_cats", "attribute_spayed_neutered", "attribute_house_trained", 
+BINARY_COLS = ["breed_mixed", "breed_unknown", "attribute_spayed_neutered", "attribute_house_trained", 
                "attribute_shots_current", "attribute_special_needs"]
