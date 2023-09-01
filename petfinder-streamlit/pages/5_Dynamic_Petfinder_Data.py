9b4c6c9aa74d3505fa984107c9b41e246ccf84c2
Jared Kunhart <Jared-Kunhart@proton.me>
2023-09-01 16:29:55 -0500

    added requirement for model

--- petfinder-streamlit/pages/5_Dynamic_Petfinder_Data.py
+++ petfinder-streamlit/pages/5_Dynamic_Petfinder_Data.py
@@ -8,22 +8,19 @@ st.set_page_config(layout="wide")
 
 token = pfglobals.get_token()
 
-
-url = "https://api.petfinder.com/v2/animals"
+url_types = "https://api.petfinder.com/v2/types"
 headers = {"Authorization": f"Bearer {token}"}
 
-#Look up params in petfinder docs https://www.petfinder.com/developers/v2/docs/ - Make them dynamic (different pets etc).
-
-#I set limit to 1 and page to 1 to not use up so many requests. Be careful with your usage, every refresh will make a get request
-#https://www.petfinder.com/user/developer-settings/
-#Today's request usage: 0 / 1,000
-#Daily usage count reset at 12:00am UTC
-#Rate Limit
-#100 requests/second
-
+response = requests.get(url_types, headers=headers)
+r = response.json()
+animals = [t for t in r['types']]
+types = [t['name'] for t in animals]
+# print(json.dumps(r, indent=2))
+print(types)
+animal_type = st.sidebar.selectbox('Select a animal.', types)
 
 params = {
-    "type": "dog",
+    "type": f"{animal_type}",
     "status": "adopted",
     "organization": None,
     "location": "Chicago, IL",
@@ -32,9 +29,8 @@ params = {
     "limit": 1,
     "page": 1,
 }
-
-response = requests.get(url, headers=headers, params=params)
-r = response.json()
-print(json.dumps(r, indent=2))
-
-# You can turn it into a data frame from here.
+# Animal type, location, if they are adopted or not
+url = "https://api.petfinder.com/v2/animals"
+type_response = requests.get(url, headers=headers, params=params)
+r_type = type_response.json()
+print(json.dumps(r_type, indent=2))
