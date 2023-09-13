4865f7353db4dea45c93d966b6614f07b5555894
Jared Kunhart <Jared-Kunhart@proton.me>
2023-09-13 00:13:34 -0500

    dark theme, increased limit and distance to max

--- petfinder-streamlit/pages/5_Dynamic_Petfinder_Data.py
+++ petfinder-streamlit/pages/5_Dynamic_Petfinder_Data.py
@@ -15,8 +15,8 @@ if "petfinder_animals" not in st.session_state:
     st.session_state['petfinder_animals'] = []
 
 st.title("Petfinder Slideshow")
-st.write("**Live data pulled from petfinder in a slideshow format. Sorted by recent. Dog's have a predictive model applied to their length of stay.**")
-
+st.markdown("Live data pulled from Petfinder in a slideshow format. If you are interested in the animal, please click the Petfinder link for more detailed information!")
+st.markdown("# ")
 token = pfglobals.get_token()
 headers = {"Authorization": f"Bearer {token}"}
 
@@ -24,19 +24,20 @@ def reset_session_state():
     st.session_state['petfinder_animals'] = []
     st.session_state['page_number'] = 0
 
-animal_type = st.sidebar.selectbox('Select an Animal Type.', ['Dog', 'Cat', 'Rabbit', 'Small & Furry', 'Horse', 'Bird', 'Scales, Fins & Other', 'Barnyard'], on_change=reset_session_state)
-adoption_status = st.sidebar.selectbox('Select Adoption Status.', ["Adoptable", "Adopted", "Found"], on_change=reset_session_state)
-location = st.sidebar.text_input('Location', help="Postal Code or City, State", value="Chicago, IL", placeholder="Chicago, IL", on_change=reset_session_state)
-
+animal_type = st.sidebar.selectbox('Select an Animal Type', ['Dog', 'Cat', 'Rabbit', 'Small & Furry', 'Horse', 'Bird', 'Scales, Fins & Other', 'Barnyard'], on_change=reset_session_state)
+adoption_status = st.sidebar.selectbox('Select Adoption Status', ['Adoptable', 'Adopted', 'Found'], on_change=reset_session_state)
+location = st.sidebar.text_input('Set a Location', help='Postal Code or City, State', value='Chicago, IL', placeholder='Chicago, IL', on_change=reset_session_state)
+sort = st.sidebar.selectbox('Sort By', ['Recent', 'Distance'], on_change=reset_session_state, help='Within 500 miles')
+sorted = 'recent' if sort == 'Recent' else 'distance'
 url = "https://api.petfinder.com/v2/animals"
 params = {
     "type": f"{animal_type}",
     "status": f"{adoption_status}",
     "organization": None,
     "location": f"{location}",
-    "sort": "recent",
-    "distance": 100,
-    "limit": 20,
+    "sort": {sorted},
+    "distance": 500,
+    "limit": 100,
     "page": 1,
 }
 
@@ -47,8 +48,11 @@ if st.session_state['petfinder_animals'] == []:
             st.session_state['petfinder_animals'] = []
         else:
             st.session_state['petfinder_animals'] = response.json()
+            # print(response.json())
     except:
         st.session_state['petfinder_animals'] = []
+    # 'pagination': {'count_per_page': 5, 'total_count': 5165, 'current_page': 1, 'total_pages': 1033,
+    #                '_links': {'next': {'href': '/v2/animals?distance=100&limit=5&location=Chicago,%20IL&page=2&sort=recent&status=Adoptable&type=Cat'}}}}
 
 page_number = st.session_state['page_number']
 col1, col2 = st.columns([1, 2])
@@ -69,6 +73,7 @@ else:
     petfinder_link = f"<a href=\"{link}\" target=\"_blank\">{name}'s Petfinder Link</a>"
     breed = animals[page_number]["breeds"]["primary"]
     description = animals[page_number]["description"] if animals[page_number]["description"] != None else "No Description Available."
+    # description = des.replace('\n', ' ')
     secondary_breed = animals[page_number]["breeds"]["secondary"] if animals[page_number]["breeds"]["secondary"] != None else ""
     size = animals[page_number]["size"] if animals[page_number]["size"] != None else ""
     location = animals[page_number]["contact"]["address"]["city"] + ", " + animals[page_number]["contact"]["address"]["state"]
@@ -86,29 +91,33 @@ else:
         if animals[page_number]["primary_photo_cropped"] == None:
             st.image("noimages.webp", use_column_width="always")
         else:
-                if len(images) > 1:
-                    carousel(items=images, width=1, height=600)
-                else:
-                    st.image(images[0]['img'], use_column_width="auto")
+            if len(images) > 1:
+                carousel(items=images, width=1, height=600)
+            else:
+                st.image(images[0]['img'], use_column_width="auto")
     with col2:
         st.subheader(f'{name} - [{name}\'s Petfinder Link]({link})', divider="rainbow")
-        st.markdown(f":blue[**Date Posted:**] {str(published_at)}")
-        if animals[page_number]["type"] == "Dog" and adoption_status == "Adoptable":
-            st.write(f":blue[**Expected Average Length of Stay:**] ")
-            st.write(f":blue[**Expected Median Length of Stay:**] ")
-        if adoption_status == "Adopted":
-            status_changed_at = pd.to_datetime(animals[page_number]["status_changed_at"])
-            published = pd.to_datetime(animals[page_number]["published_at"])
-            los = (status_changed_at - published).days
-            st.markdown(f":blue[**Length of Stay:**] {los} days")
-        st.markdown(f":blue[**Breed:**] {breed}")
-        st.markdown(f":blue[**Secondary Breed:**] {secondary_breed}")
-        st.markdown(f":blue[**Age:**] {age}")
-        st.markdown(f":blue[**Gender:**] {gender}")
-        st.markdown(f":blue[**Size:**] {size}")
-        st.markdown(f":blue[**Location:**] {location}")
-        st.markdown(f":blue[**Description:**] {description}")
-        st.markdown("# ")
+        col3, col4 = st.columns([1, 2])
+        with col3:
+            st.markdown(f":blue[**Date Posted:**]  {str(published_at)}")
+            if animals[page_number]["type"] == "Dog" and adoption_status == "Adoptable":
+                st.write(f":blue[**Expected Average Length of Stay:**]  ")
+                st.write(f":blue[**Expected Median Length of Stay:**]  ")
+            if adoption_status == "Adopted":
+                status_changed_at = pd.to_datetime(animals[page_number]["status_changed_at"])
+                published = pd.to_datetime(animals[page_number]["published_at"])
+                los = (status_changed_at - published).days
+                st.markdown(f":blue[**Length of Stay:**]  {los} days")
+            st.markdown(f":blue[**Breed:**]  {breed}")
+            st.markdown(f":blue[**Secondary Breed:**]  {secondary_breed}")
+            st.markdown(f":blue[**Age:**]  {age}")
+            st.markdown(f":blue[**Gender:**]  {gender}")
+            st.markdown(f":blue[**Size:**]  {size}")
+            st.markdown(f":blue[**Location:**]  {location}")
+        with col4:
+            st.markdown(":blue[**Description:**]")
+            st.caption(f"{description}")
+            st.markdown("# ")
         _, prev ,next = st.columns([2, 10, 5])
         if int(page_number) + 1 == int(last_page):
             is_last_page = True
@@ -120,11 +129,11 @@ else:
         else:
             is_first_page = False
 
+
         def next_page():
             st.session_state['page_number'] += 1
 
         next.button("Next", on_click=next_page, disabled=is_last_page)
-
         def prev_page():
             st.session_state['page_number'] -= 1
 
