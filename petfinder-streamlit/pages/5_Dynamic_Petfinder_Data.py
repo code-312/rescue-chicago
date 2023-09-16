7071d2c52451e216d85651d3fcb79b9e31a7d1d6
Jared Kunhart <Jared-Kunhart@proton.me>
2023-09-15 23:56:43 -0500

    removed carousel, fixed formatting, added conditionals, just needs a model now

--- petfinder-streamlit/pages/5_Dynamic_Petfinder_Data.py
+++ petfinder-streamlit/pages/5_Dynamic_Petfinder_Data.py
@@ -1,24 +1,20 @@
-import os
 import streamlit as st
-from streamlit_carousel import carousel
 from datetime import datetime
 import pfglobals
 import requests
-import json
 import pandas as pd
 
 st.set_page_config(page_title="Pawsitive Finder", page_icon="🐇", layout="wide")
-"""
 
-"""
 if "page_number" not in st.session_state:
     st.session_state['page_number'] = 0
 if "petfinder_animals" not in st.session_state:
     st.session_state['petfinder_animals'] = []
 
-st.title("Pawsitive Finder")
+st.title(":blue[Pawsitive Finder]")
 st.markdown("Live data pulled from Petfinder in a slideshow format. If you are interested in the animal, please click the Petfinder link for more detailed information!")
 st.markdown("# ")
+
 token = pfglobals.get_token()
 headers = {"Authorization": f"Bearer {token}"}
 
@@ -50,11 +46,8 @@ if st.session_state['petfinder_animals'] == []:
             st.session_state['petfinder_animals'] = []
         else:
             st.session_state['petfinder_animals'] = response.json()
-            # print(response.json())
     except:
         st.session_state['petfinder_animals'] = []
-    # 'pagination': {'count_per_page': 5, 'total_count': 5165, 'current_page': 1, 'total_pages': 1033,
-    #                '_links': {'next': {'href': '/v2/animals?distance=100&limit=5&location=Chicago,%20IL&page=2&sort=recent&status=Adoptable&type=Cat'}}}}
 
 page_number = st.session_state['page_number']
 col1, col2 = st.columns([1, 2])
@@ -74,69 +67,60 @@ else:
     name = animals[page_number]["name"]
     petfinder_link = f"<a href=\"{link}\" target=\"_blank\">{name}'s Petfinder Link</a>"
     breed = animals[page_number]["breeds"]["primary"]
+    spayed_neutered = animals[page_number]["attributes"]["spayed_neutered"]
     description = animals[page_number]["description"] if animals[page_number]["description"] != None else "No Description Available."
-    # description = des.replace('\n', ' ')
     secondary_breed = animals[page_number]["breeds"]["secondary"] if animals[page_number]["breeds"]["secondary"] != None else ""
-    size = animals[page_number]["size"] if animals[page_number]["size"] != None else ""
     location = animals[page_number]["contact"]["address"]["city"] + ", " + animals[page_number]["contact"]["address"]["state"]
-    published_at = animals[page_number]["published_at"][:10]
-    images = []
-    if animals[page_number]["primary_photo_cropped"] != None:
-        for image in animals[page_number]["photos"]:
-            images.append(dict(
-                interval=5000,
-                img=image['large'],
-                title=name,
-                text=breed,
-            ))
     with col1:
         if animals[page_number]["primary_photo_cropped"] == None:
             st.image("noimages.webp", use_column_width="always")
         else:
-            if len(images) > 1:
-                carousel(items=images, width=1, height=600)
-            else:
-                st.image(images[0]['img'], use_column_width="auto")
+            image = animals[page_number]['photos'][0]['large']
+            st.image(image, use_column_width="auto")
+            st.markdown("""
+            <style>
+            .css-1v0mbdj img {
+                width: 425px !important;
+                height: 425px !important;
+                object-fit: cover !important;
+                border-radius: 10px !important;
+            }
+            </style>""", unsafe_allow_html=True
+            )
     with col2:
         st.subheader(f'{name} - [{name}\'s Petfinder Link]({link})', divider="rainbow")
-        col3, col4 = st.columns([1, 2])
-        with col3:
-            st.markdown(f":blue[**Date Posted:**]  {str(published_at)}")
-            if animals[page_number]["type"] == "Dog" and adoption_status == "Adoptable":
-                st.write(f":blue[**Expected Average Length of Stay:**]  ")
-                st.write(f":blue[**Expected Median Length of Stay:**]  ")
-            if adoption_status == "Adopted":
-                status_changed_at = pd.to_datetime(animals[page_number]["status_changed_at"])
-                published = pd.to_datetime(animals[page_number]["published_at"])
-                los = (status_changed_at - published).days
-                st.markdown(f":blue[**Length of Stay:**]  {los} days")
-            st.markdown(f":blue[**Breed:**]  {breed}")
-            st.markdown(f":blue[**Secondary Breed:**]  {secondary_breed}")
-            st.markdown(f":blue[**Age:**]  {age}")
-            st.markdown(f":blue[**Gender:**]  {gender}")
-            st.markdown(f":blue[**Size:**]  {size}")
-            st.markdown(f":blue[**Location:**]  {location}")
-        with col4:
-            st.markdown(":blue[**Description:**]")
-            st.caption(f"{description}")
-            st.markdown("# ")
-        _, prev ,next = st.columns([2, 10, 5])
-        if int(page_number) + 1 == int(last_page):
-            is_last_page = True
+        st.markdown(f"#### {breed} :gray[•] {gender} :gray[•] {age}")
+        if secondary_breed != "":
+            st.markdown(f":blue[**Secondary Breed:**] {secondary_breed}")
+        # if animals[page_number]["type"] == "Dog" and adoption_status == "Adoptable":
+        #     st.text(f"Median Length of Stay: ")
+        if spayed_neutered == True:
+            st.markdown(f":blue[**Spayed or Neutered:**] Yes")
         else:
-            is_last_page = False
+            st.markdown(f":blue[**Spayed or Neutered:**] No")
+        st.markdown(f":blue[**Location:**] {location}")
+        if adoption_status == "Adopted":
+            status_changed_at = pd.to_datetime(animals[page_number]["status_changed_at"])
+            published = pd.to_datetime(animals[page_number]["published_at"])
+            los = (status_changed_at - published).days
+            st.markdown(f":blue[**Length of Stay:**]  {los} days")
+        st.code(f"Description:\n{description}")
+_, prev, next = st.columns([6, 5, 7])
+if int(page_number) + 1 == int(last_page):
+    is_last_page = True
+else:
+    is_last_page = False
 
-        if int(page_number) == 0:
-            is_first_page = True
-        else:
-            is_first_page = False
+if int(page_number) == 0:
+    is_first_page = True
+else:
+    is_first_page = False
 
+def next_page():
+    st.session_state['page_number'] += 1
 
-        def next_page():
-            st.session_state['page_number'] += 1
+next.button("Next", on_click=next_page, disabled=is_last_page)
+def prev_page():
+    st.session_state['page_number'] -= 1
 
-        next.button("Next", on_click=next_page, disabled=is_last_page)
-        def prev_page():
-            st.session_state['page_number'] -= 1
-
-        prev.button("Previous", on_click=prev_page, disabled=is_first_page)
+prev.button("Previous", on_click=prev_page, disabled=is_first_page)
